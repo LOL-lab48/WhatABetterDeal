4d4cec0c471086d54c310f0d0c3540214cc68fce
LOL-lab48 <32gk@student.nbcs.nsw.edu.au>
2026-01-09 18:30:12 +1100

    Fix display style quotes and add openAdModal function

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// Open item modal by ID
 function openModal(id) {
-  document.getElementById(id).style.display='flex';
+  document.getElementById(id).style.display = "flex";
 }
 
+// Close item modal by ID
 function closeModal(id) {
-  document.getElementById(id).style.display='none';
+  document.getElementById(id).style.display = "none";
+}
+
+// Open ad modal by ID
+function openAdModal(id) {
+  openModal(id);
 }
 
 // Show thank you message after form submission
-document.querySelector("form").addEventListener("submit", function(e){
-  setTimeout(()=>{
-    document.getElementById('thankYouMessage').style.display='block';
+document.querySelector("form").addEventListener("submit", function () {
+  setTimeout(() => {
+    document.getElementById("thankYouMessage").style.display = "block";
   }, 200);
 });
+
 
