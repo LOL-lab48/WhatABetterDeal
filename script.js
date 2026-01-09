1af4392eaf539216a0b199b80154d198d685b9f5
LOL-lab48 <32gk@student.nbcs.nsw.edu.au>
2026-01-09 18:13:45 +1100

    Fix form submission event handler for thank you message

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function openModal(id) {
   document.getElementById(id).style.display='flex';
 }
+
 function closeModal(id) {
   document.getElementById(id).style.display='none';
 }
 
 // Show thank you message after form submission
-document.querySelector("form").addEventListener("submit", function(){
-  setTimeout(()=>{document.getElementById('thankYouMessage').style.display='block';}, 200);
+document.querySelector("form").addEventListener("submit", function(e){
+  setTimeout(()=>{
+    document.getElementById('thankYouMessage').style.display='block';
+  }, 200);
 });
+
